6e88352d79c07b86e0213adf32a461de138766e8
Sam Panganiban <sam_panganiban@Sams-MacBook-Pro.local>
2015-08-11 23:01:43 +1200

    restyled the login page

--- www/templates/login.php
+++ www/templates/login.php
@@ -5,23 +5,25 @@
                 <h2 class="section-heading text-center">Login to your Account</h2>
                 <h3 class="section-subheading text muted text-center">Logging into your account helps to make placing orders easy for you!</h3>
             </div>
-            <div class="row">
+            <div class="row" class="col-md-12">
                 <form novalidate method="post" action="index.php?page=login">
 
-                <div class="form-group">
+                <div class="col-md-5">
                     <label for="username">Username:</label>
                     <input type="text" name="username" id="username" value="<?php echo $this->username; ?>">
                     <?php $this->bootstrapAlert($this->usernameError, 'danger') ?>
                 </div>
                 
-                <div class="form-group">
+                <div class="col-md-5">
                     <label for="password">Password:</label>
                     <input type="password" name="password" id="password">
                     <?php $this->bootstrapAlert($this->passwordError, 'danger') ?>
                 </div>
                 
+                <div class="col-md-2">
                     <button type="submit" class="btn btn-primary" name="login">Login</button>
                     <?php $this->bootstrapAlert($this->loginError, 'danger') ?>
+                </div>
                 
                 </form>
             </div>
